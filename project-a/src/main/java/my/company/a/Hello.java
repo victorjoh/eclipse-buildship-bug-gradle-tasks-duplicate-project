3769b473b74a09c48a41460cd6b9da9383a31a36
Victor Johnsson <victor.johnsson@unifaun.com>
2022-03-08 08:59:06 +0100

    initial commit

---
+++ project-a/src/main/java/my/company/a/Hello.java
@@ -0,0 +1,8 @@
+package my.company.a;
+
+public class Hello {
+
+    public static String hello() {
+        return "Hello";
+    }
+}
---
+++ project-b/src/main/java/my/company/b/World.java
@@ -0,0 +1,10 @@
+package my.company.b;
+
+import my.company.a.Hello;
+
+public class World {
+
+    public static void main(String[] args) {
+        System.out.println(Hello.hello() + ", world!");
+    }
+}
